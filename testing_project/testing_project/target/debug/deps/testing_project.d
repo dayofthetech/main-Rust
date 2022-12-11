c71c1134f3d3b94b2a02065e02b66edd2001c44e
dayofthetech <j.alberto.reyes.r90@gmail.com>
2022-12-11 14:57:53 -0500

    creating second folder

---
+++ testing_project/testing_project/target/debug/deps/testing_project.d
@@ -0,0 +1,5 @@
+C:\Users\alber\Documents\main Rust\main-Rust\testing_project\testing_project\target\debug\deps\testing_project.exe: src\main.rs
+
+C:\Users\alber\Documents\main Rust\main-Rust\testing_project\testing_project\target\debug\deps\testing_project.d: src\main.rs
+
+src\main.rs:
---
+++ testing_project/testing_project/target/debug/testing_project.d
@@ -0,0 +1,1 @@
+C:\Users\alber\Documents\main\ Rust\main-Rust\testing_project\testing_project\target\debug\testing_project.exe: C:\Users\alber\Documents\main\ Rust\main-Rust\testing_project\testing_project\src\main.rs
