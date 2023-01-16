0a9c1f368d80c9abcd591322b6254329c7f6b161
Alberto <61217370+dayofthetech@users.noreply.github.com>
2023-01-16 14:40:00 -0500

    Update tuples.rs

--- rust_crash_course/src/tuples.rs
+++ rust_crash_course/src/tuples.rs
@@ -6,3 +6,11 @@ pub fn run() {
     println!("{} is from {} and age", person.0, person.1);
 
 }
+
+//Another example
+fn main() {
+    let cat = ("Furry McFurson", 3.5);
+    let (name, age)/* your pattern here */ = cat;
+
+    println!("{} is {} years old.", name, age);
+}
